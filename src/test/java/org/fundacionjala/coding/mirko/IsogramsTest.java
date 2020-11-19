248229203e8f621ba9a77c69dadf1527a9f79f0b
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-11-19 14:00:18 -0400

    Solving 'Isograms' exercise using TDD

--- src/test/java/org/fundacionjala/coding/mirko/IsogramsTest.java
+++ src/test/java/org/fundacionjala/coding/mirko/IsogramsTest.java
@@ -3,32 +3,48 @@ package org.fundacionjala.coding.mirko;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+/**
+ * This class contains basic tests for 'Isograms' exercise.
+ */
 public class IsogramsTest {
+
+    /**
+     * First i/o example present at https://www.codewars.com/kata/54ba84be607a92aa900000f1.
+     */
     @Test
     public void basicFirstInputTest() {
         String input = "Dermatoglyphics";
-        Boolean actual = Isograms.verify(input);
+        Boolean actual = Isograms.isIsogram(input);
         Assert.assertTrue(actual);
     }
 
+    /**
+     * Second i/o example present at https://www.codewars.com/kata/54ba84be607a92aa900000f1.
+     */
     @Test
     public void basicSecondInputTest() {
         String input = "aba";
-        Boolean actual = Isograms.verify(input);
+        Boolean actual = Isograms.isIsogram(input);
         Assert.assertFalse(actual);
     }
 
+    /**
+     * Third i/o example present at https://www.codewars.com/kata/54ba84be607a92aa900000f1.
+     */
     @Test
     public void basicThirdInputTest() {
         String input = "moOse";
-        Boolean actual = Isograms.verify(input);
+        Boolean actual = Isograms.isIsogram(input);
         Assert.assertFalse(actual);
     }
 
+    /**
+     * Test that verify if the method works correctly for an empty String.
+     */
     @Test
-    public void EmptyInputTest() {
+    public void emptyInputTest() {
         String input = "";
-        Boolean actual = Isograms.verify(input);
+        Boolean actual = Isograms.isIsogram(input);
         Assert.assertTrue(actual);
     }
 }
