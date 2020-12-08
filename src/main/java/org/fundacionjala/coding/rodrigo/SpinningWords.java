134739910039de6f5b9df81d16de72500ce7dbf1
roht0515 <53868792+roht0515@users.noreply.github.com>
2020-12-08 10:49:43 -0400

    [develop] Rodrigo Huanca - Stop spinning my sdrow (#4)
    * Spinning words class and methods created
    
    * Tests for Spinning class added
    
    * modifiying a text param

---
+++ src/main/java/org/fundacionjala/coding/rodrigo/SpinningWords.java
@@ -0,0 +1,41 @@
+package org.fundacionjala.coding.rodrigo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Spinning Words class.
+ */
+public final class SpinningWords {
+
+    /**
+     * Condition for reversing words.
+     */
+    private static final int LIMIT = 5;
+
+    /**
+     * Private constructor for utility class.
+     */
+    private SpinningWords() {
+
+    }
+
+    /**
+     * Spin the words with more letters than the limit.
+     *
+     * @param input string message input.
+     * @return message with the spin words if possible.
+     */
+    public static String spinningMessage(final String input) {
+        List<String> message = Arrays.asList(input.split(" "));
+        message.stream().forEach((word) -> {
+            if (word.length() >= LIMIT) {
+                StringBuilder builder = new StringBuilder(word);
+                String reverse = builder.reverse().toString();
+                message.set(message.indexOf(word), reverse);
+            }
+        });
+        String str = String.join(" ", message);
+        return str;
+    }
+}
